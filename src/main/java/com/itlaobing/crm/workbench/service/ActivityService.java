b259a5d5b18cb9aeb5345e3e9ddbb7b5977f7ef2
tom_glb <helloworld_glb@qq.com>
2020-11-24 22:59:08 +0800

    crm第五天进度

--- src/main/java/com/itlaobing/crm/workbench/service/ActivityService.java
+++ src/main/java/com/itlaobing/crm/workbench/service/ActivityService.java
@@ -1,7 +1,12 @@
 package com.itlaobing.crm.workbench.service;
 
+import com.itlaobing.crm.vo.PaginationVO;
 import com.itlaobing.crm.workbench.domain.Activity;
+
+import java.util.Map;
 
 public interface ActivityService {
     boolean save(Activity activity);
+
+    PaginationVO<Activity> pageList(Map<String, Object> map);
 }
