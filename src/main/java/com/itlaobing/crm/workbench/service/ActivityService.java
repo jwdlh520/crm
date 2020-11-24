aa813a2accc7231a84839cf548b9d9b329f6e879
tom_glb <helloworld_glb@qq.com>
2020-11-24 19:07:28 +0800

    crm第四天进度

--- src/main/java/com/itlaobing/crm/workbench/service/ActivityService.java
+++ src/main/java/com/itlaobing/crm/workbench/service/ActivityService.java
@@ -1,4 +1,7 @@
 package com.itlaobing.crm.workbench.service;
 
+import com.itlaobing.crm.workbench.domain.Activity;
+
 public interface ActivityService {
+    boolean save(Activity activity);
 }
--- src/main/java/com/itlaobing/crm/workbench/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/itlaobing/crm/workbench/service/impl/ActivityServiceImpl.java
@@ -2,8 +2,19 @@ package com.itlaobing.crm.workbench.service.impl;
 
 import com.itlaobing.crm.utils.SqlSessionUtil;
 import com.itlaobing.crm.workbench.dao.ActivityDao;
+import com.itlaobing.crm.workbench.domain.Activity;
 import com.itlaobing.crm.workbench.service.ActivityService;
 
 public class ActivityServiceImpl implements ActivityService {
     private ActivityDao dao = SqlSessionUtil.getSqlSession().getMapper(ActivityDao.class);
+
+    @Override
+    public boolean save(Activity activity) {
+        Integer count = dao.save(activity);
+        boolean flag = true;
+        if (count != 1){
+            flag = false;
+        }
+        return flag;
+    }
 }
