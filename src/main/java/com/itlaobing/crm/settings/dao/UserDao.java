57b2267d3d13f164af7fa0c0297d1b35a00f98e7
tom_glb <helloworld_glb@qq.com>
2020-11-24 19:05:09 +0800

    crm第四天进度

--- src/main/java/com/itlaobing/crm/settings/dao/UserDao.java
+++ src/main/java/com/itlaobing/crm/settings/dao/UserDao.java
@@ -2,8 +2,11 @@ package com.itlaobing.crm.settings.dao;
 
 import com.itlaobing.crm.settings.domain.User;
 
+import java.util.List;
 import java.util.Map;
 
 public interface UserDao {
     User login(Map<String, String> map);
+
+    List<User> queryUserAll();
 }
