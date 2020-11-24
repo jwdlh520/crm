c4f513e236d794cd384ee1f6107c7c750a073b6b
tom_glb <helloworld_glb@qq.com>
2020-11-24 22:55:53 +0800

    crm第五天进度

--- src/main/java/com/itlaobing/crm/workbench/dao/ActivityDao.java
+++ src/main/java/com/itlaobing/crm/workbench/dao/ActivityDao.java
@@ -1,7 +1,17 @@
 package com.itlaobing.crm.workbench.dao;
 
+import com.itlaobing.crm.vo.PaginationVO;
 import com.itlaobing.crm.workbench.domain.Activity;
+
+import java.util.List;
+import java.util.Map;
 
 public interface ActivityDao {
     Integer save(Activity activity);
+
+    PaginationVO<Activity> pageList(Map<String, Object> map);
+
+    Integer getTotalByCondition(Map<String, Object> map);
+
+    List<Activity> getDataListByCondition(Map<String, Object> map);
 }
