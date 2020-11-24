95e21a27c4651af247b86beb2836cc90548922d4
tom_glb <helloworld_glb@qq.com>
2020-11-24 22:56:05 +0800

    crm第五天进度

--- src/main/java/com/itlaobing/crm/workbench/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/itlaobing/crm/workbench/service/impl/ActivityServiceImpl.java
@@ -1,9 +1,13 @@
 package com.itlaobing.crm.workbench.service.impl;
 
 import com.itlaobing.crm.utils.SqlSessionUtil;
+import com.itlaobing.crm.vo.PaginationVO;
 import com.itlaobing.crm.workbench.dao.ActivityDao;
 import com.itlaobing.crm.workbench.domain.Activity;
 import com.itlaobing.crm.workbench.service.ActivityService;
+
+import java.util.List;
+import java.util.Map;
 
 public class ActivityServiceImpl implements ActivityService {
     private ActivityDao dao = SqlSessionUtil.getSqlSession().getMapper(ActivityDao.class);
@@ -16,5 +20,20 @@ public class ActivityServiceImpl implements ActivityService {
             flag = false;
         }
         return flag;
+    }
+
+    @Override
+    public PaginationVO<Activity> pageList(Map<String, Object> map) {
+
+        PaginationVO<Activity> paginationVO = new PaginationVO<>();
+        //获取总数
+        Integer total = dao.getTotalByCondition(map);
+        System.out.println(total);
+        //获取信息列表
+        List<Activity> list = dao.getDataListByCondition(map);
+        System.out.println(list.size());
+        paginationVO.setTotal(total);
+        paginationVO.setDataList(list);
+        return paginationVO;
     }
 }
