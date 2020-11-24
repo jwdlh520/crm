f401dbd274014cbbeb084398c6dd0c442e4d9471
tom_glb <helloworld_glb@qq.com>
2020-11-24 19:07:50 +0800

    crm第四天进度

--- src/main/java/com/itlaobing/crm/workbench/web/controller/ActivityController.java
+++ src/main/java/com/itlaobing/crm/workbench/web/controller/ActivityController.java
@@ -3,8 +3,10 @@ package com.itlaobing.crm.workbench.web.controller;
 import com.itlaobing.crm.settings.domain.User;
 import com.itlaobing.crm.settings.service.UserService;
 import com.itlaobing.crm.settings.service.impl.UserServiceImpl;
-import com.itlaobing.crm.utils.PrintJson;
-import com.itlaobing.crm.utils.ServiceFactory;
+import com.itlaobing.crm.utils.*;
+import com.itlaobing.crm.workbench.domain.Activity;
+import com.itlaobing.crm.workbench.service.ActivityService;
+import com.itlaobing.crm.workbench.service.impl.ActivityServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -20,15 +22,45 @@ public class ActivityController extends HttpServlet {
     public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("进入活动操作");
         String path = request.getServletPath();
-        if ("/workbench/activity/QueryUserAll.do".equals(path)){
-            List<User> uList = queryUserAll();
-            System.out.println(uList.toString());
-            PrintJson.printJsonObj(response,uList);
+        if ("/workbench/activity/QueryUserAll.do".equals(path)) {
+            queryUserAll(request, response);
+
+        } else if ("/workbench/activity/save.do".equals(path)) {
+            //id自动生成
+            save(request, response);
         }
     }
-    private List<User> queryUserAll() {
+
+    private void queryUserAll(HttpServletRequest request, HttpServletResponse response) {
         UserService userService = (UserService) ServiceFactory.getService(new UserServiceImpl());
         List<User> users = userService.queryUserAll();
-        return users;
+        PrintJson.printJsonObj(response, users);
+    }
+
+    private void save(HttpServletRequest request, HttpServletResponse response) {
+        String id = UUIDUtil.getUUID();
+        String owner = request.getParameter("owner");
+        String name = request.getParameter("name");
+        String startDate = request.getParameter("startDate");
+        String endDate = request.getParameter("endDate");
+        String cost = request.getParameter("cost");
+        String description = request.getParameter("description");
+        //当前系统时间
+        String createTime = DateTimeUtil.getSysTime();
+        //当前登录用户
+        String createBy = ((User) request.getSession().getAttribute("user")).getName();
+        Activity activity = new Activity();
+        activity.setId(id);
+        activity.setOwner(owner);
+        activity.setName(name);
+        activity.setStartDate(startDate);
+        activity.setEndDate(endDate);
+        activity.setCost(cost);
+        activity.setDescription(description);
+        activity.setCreateTime(createTime);
+        activity.setCreateBy(createBy);
+        ActivityService activityService = (ActivityService) ServiceFactory.getService(new ActivityServiceImpl());
+        boolean flag = activityService.save(activity);
+        PrintJson.printJsonFlag(response, flag);
     }
 }
